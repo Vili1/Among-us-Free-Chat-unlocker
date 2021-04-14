df037026551804ed27f208086f87ce8c67e8fe6f
Viliyan Yordanov <42891941+Vili1@users.noreply.github.com>
2021-04-14 04:17:15 +0300

    support for cracked version v2021.4.1s

--- src/chat.cpp
+++ src/chat.cpp
@@ -69,8 +69,8 @@ int main()
 
     char moduleName[] = "GameAssembly.dll";
     DWORD gameBaseAddress = dwGetModuleBaseAddress(_T(moduleName), pID);
-    DWORD offsetGameToBaseAddress = 0x028A83D4;
-    std::vector<DWORD> pointsOffsets{0x5C, 0x2C};
+    DWORD offsetGameToBaseAddress = 0x029AC514;
+    std::vector<DWORD> pointsOffsets{0xAC};
     DWORD baseAddress;
 
     ReadProcessMemory(processHandle, (LPVOID)(gameBaseAddress + offsetGameToBaseAddress), &baseAddress, sizeof(baseAddress), NULL);
