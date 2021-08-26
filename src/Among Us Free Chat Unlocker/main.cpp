2ae065f667f2957c27424a9c0be150634f7b793b
Viliyan Yordanov <42891941+Vili1@users.noreply.github.com>
2021-08-26 23:40:08 +0300

    Added information

--- src/Among Us Free Chat Unlocker/main.cpp
+++ src/Among Us Free Chat Unlocker/main.cpp
@@ -8,8 +8,27 @@ int main()
 {
 	//Get Process ID by enumerating the processes using tlhelp32snapshot
 	DWORD processID = GetProcID(L"Among Us.exe");
+	if (processID != NULL)
+	{
+		std::cout << "Among Us found successfully!\n";
+		std::cout << "---------------------------------------------------------------------------\n";
+	}
+	else
+	{
+		std::cout << "Please make sure that Among Us is running!\n";
+		std::cout << "---------------------------------------------------------------------------\n";
+		system("pause");
+		return  0;
+	}
 	//Get handle by OpenProcess
 	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, processID);
+	if (hProcess == INVALID_HANDLE_VALUE || hProcess == NULL)
+	{
+		std::cout << "Try to run the application as administrator.\n";
+		std::cout << "---------------------------------------------------------------------------\n";
+		system("pause");
+		return  0;
+	}
 
 	//PatternScan
 	void* chatAddr = PatternScanExModule(hProcess, L"Among Us.exe", L"GameAssembly.dll", "\x8b\x40\x00\xc3\xcc\xcc\x80\x3d", "xx?xxxxx");
@@ -18,7 +37,8 @@ int main()
 	if (chatAddr != nullptr)
 	{
 		NopEx(hProcess, chatAddr, 3);
-		std::cout << "Unlocked successfully!\n";
+		std::cout << "The chat has been unlocked successfully!\n";
+		std::cout << "---------------------------------------------------------------------------\n";
 		system("pause");
 	}
 
