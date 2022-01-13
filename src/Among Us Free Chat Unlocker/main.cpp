ad74eade9d6c21ef36efcad1eba52bb4e57ff59f
Viliyan Yordanov <42891941+Vili1@users.noreply.github.com>
2022-01-13 20:04:59 +0200

    NAME CHANGER AND ABILITY TO JOIN PRIVATE ROOMS WITH FREE CHAT

--- src/Among Us Free Chat Unlocker/main.cpp
+++ src/Among Us Free Chat Unlocker/main.cpp
@@ -30,19 +30,37 @@ int main()
 		return  0;
 	}
 
-	//PatternScan
-	void* chatAddr = PatternScanExModule(hProcess, L"Among Us.exe", L"GameAssembly.dll", "\x83\x78\x2C\x00\x75\x2E", "xxx?xx");
+	std::cout << "Make sure that you are in offline mode before you press enter!\n";
+	std::cout << "---------------------------------------------------------------------------\n";
+	system("pause");
+	//PatternScan for the opcode that writes 2 to the chat value if its set to quick chat
+	void* onlineAddr = PatternScanExModule(hProcess, L"Among Us.exe", L"GameAssembly.dll", "\x89\x41\x2C\xC7\x45\x0C\x00\x00\x00\x00\xC7", "xxxxxxxxxxx");
 
+	//patch the instruction
+	if (onlineAddr != nullptr)
+	{
+		// patches the instruction to write 2 in the play as guest value which tricks the game to let you change your name
+		byte patchOnline[3] = { 0x89, 0x41, 0x38 };
+		PatchEx(hProcess, onlineAddr, patchOnline, sizeof(patchOnline));
+		std::cout << "The name changer has been unlocked successfully!\n";
+		std::cout << "Please sing in and then change your name!\n";
+		std::cout << "---------------------------------------------------------------------------\n";
+
+	}
+
+	//PatternScan for the chat compare opcode 
+	void* chatAddr = PatternScanExModule(hProcess, L"Among Us.exe", L"GameAssembly.dll", "\x83\x78\x2C\x00\x75\x2E", "xxx?xx");
+	
 	//patch the instruction
 	if (chatAddr != nullptr)
 	{
 		// patches the instruction that compares the chat mode to compare to 2 which is the restricted chat
-		byte patchArray[4] = { 0x83, 0x78, 0x2C, 0x02 };
-		PatchEx(hProcess, chatAddr, patchArray, sizeof(patchArray));
+		byte patchChat[4] = { 0x83, 0x78, 0x2C, 0x02 };
+		PatchEx(hProcess, chatAddr, patchChat, sizeof(patchChat));
 		std::cout << "The chat has been unlocked successfully!\n";
 		std::cout << "---------------------------------------------------------------------------\n";
 		system("pause");
 	}
-
+	
 	return  0;
 }
